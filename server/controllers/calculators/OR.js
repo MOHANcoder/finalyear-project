44094bf6d60c8c133c5d315fa4caadfa705e40ae
MOHANcoder <mohan235689@gmail.com>
2024-01-10 13:28:59 +0530

    Transportation Solver route added

--- server/controllers/calculators/OR.js
+++ server/controllers/calculators/OR.js
@@ -1,6 +1,7 @@
 const {
     Assignment,
-    HTMLOutputGenerator
+    HTMLOutputGenerator,
+    Transportation
 } = require("operations-research");
 
 module.exports = {
@@ -25,6 +26,32 @@ module.exports = {
             data:{
                 cost,
                 htmlContent : htmlGenerator.getContent()
+            }
+        });
+    },
+    calculateTransportation : (req,res,next) => {
+        const {costTable,origins,destinations,method} = req.body;
+        const htmlGenerator = new HTMLOutputGenerator("TRANSPORTATION");
+        const transportationSolver = new Transportation(costTable,origins,destinations,htmlGenerator);
+        let cost;
+        try{
+            switch(method){
+                case "VA":transportationSolver.findIBFSUsingVogelApproximation();
+                break;
+            }
+        }catch(error){
+            return res.status(400).json({
+                success:false,
+                message:"Error during calculations",
+                data:{}
+            });
+        }
+
+        return res.status(200).json({
+            sucess:true,
+            message:"Transportation problem solved",
+            data:{
+                htmlContent:htmlGenerator.getContent()
             }
         });
     }
