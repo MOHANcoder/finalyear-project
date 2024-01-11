bdec9e55d4c8e81aedc58c014ac4728cd1b9b021
MOHANcoder <mohan235689@gmail.com>
2024-01-11 11:28:30 +0530

    update or.js

--- server/controllers/calculators/OR.js
+++ server/controllers/calculators/OR.js
@@ -3,74 +3,107 @@ const {
     HTMLOutputGenerator,
     Transportation,
     ObjectiveFunction,
-    MinifiedNumber
+    MinifiedNumber,
+    Constraint
 } = require("operations-research");
 
 module.exports = {
-    calculateAssignment: (req, res, next)=>{
-        const {costTable} = req.body;
+    calculateAssignment: (req, res, next) => {
+        const { costTable } = req.body;
         const htmlGenerator = new HTMLOutputGenerator("ASSIGNMENT");
-        const assignmentSolver = new Assignment(costTable,htmlGenerator);
+        const assignmentSolver = new Assignment(costTable, htmlGenerator);
         let cost;
-        try{
-            [_,cost,_] = assignmentSolver.solve();
-        }catch(error){
+        try {
+            [_, cost, _] = assignmentSolver.solve();
+        } catch (error) {
             return res.status(400).json({
-                success:false,
-                message:"Error during calculations",
-                data:{}
+                success: false,
+                message: "Error during calculations",
+                data: {}
             });
         }
 
         return res.status(200).json({
-            success:true,
-            message:"Assignment Done",
-            data:{
+            success: true,
+            message: "Assignment Done",
+            data: {
                 cost,
-                htmlContent : htmlGenerator.getContent()
+                htmlContent: htmlGenerator.getContent()
             }
         });
     },
-    calculateTransportation : (req,res,next) => {
-        const {costTable,origins,destinations,method} = req.body;
+    calculateTransportation: (req, res, next) => {
+        const { costTable, origins, destinations, method } = req.body;
         const htmlGenerator = new HTMLOutputGenerator("TRANSPORTATION");
-        const transportationSolver = new Transportation(costTable,origins,destinations,htmlGenerator);
+        const transportationSolver = new Transportation(costTable, origins, destinations, htmlGenerator);
         let cost;
-        try{
-            switch(method){
-                case "VA":transportationSolver.findIBFSUsingVogelApproximation();
-                break;
-                case "NW":transportationSolver.findIBFSUsingNorthWestCorner();
-                break;
-                case "RM":transportationSolver.findIBFSUsingRowMinima();
-                break;
-                case "CM":transportationSolver.findIBFSUsingColumnMinima();
-                break;
-                case "MM":transportationSolver.findIBFSUsingMatrixMinima();
-                break;
+        try {
+            switch (method) {
+                case "VA": transportationSolver.findIBFSUsingVogelApproximation();
+                    break;
+                case "NW": transportationSolver.findIBFSUsingNorthWestCorner();
+                    break;
+                case "RM": transportationSolver.findIBFSUsingRowMinima();
+                    break;
+                case "CM": transportationSolver.findIBFSUsingColumnMinima();
+                    break;
+                case "MM": transportationSolver.findIBFSUsingMatrixMinima();
+                    break;
             }
-        }catch(error){
+        } catch (error) {
             return res.status(400).json({
-                success:false,
-                message:"Error during calculations",
-                data:{}
+                success: false,
+                message: "Error during calculations",
+                data: {}
             });
         }
 
         return res.status(200).json({
-            sucess:true,
-            message:"Transportation problem solved",
-            data:{
-                htmlContent:htmlGenerator.getContent()
+            sucess: true,
+            message: "Transportation problem solved",
+            data: {
+                htmlContent: htmlGenerator.getContent()
             }
         });
     },
-    // calculateSimplex : (req,res,next) => {
-    //     const {objectiveCoefficients,optimizeType,constraints} = req.body;
-    //     const htmlGenerator = new HTMLOutputGenerator("SIMPLEX");
-    //     const simplexSolver = new ObjectiveFunction(objectiveConstraints.length,
-    //         objectiveCoefficients.map(coefficient => new MinifiedNumber(coefficient)),
-    //         constraints.map()
-    //         )
-    // }
+    calculateSimplex: (req, res, next) => {
+        const { noOfDecisionVariables,objectiveCoefficients, optimizeType, constraints ,nonNegativeConstraints} = req.body;
+        const htmlGenerator = new HTMLOutputGenerator("SIMPLEX");
+        const simplexSolver = new ObjectiveFunction(
+            noOfDecisionVariables,
+            optimizeType,
+            objectiveCoefficients.map(coefficient => new MinifiedNumber(coefficient)),
+            constraints.map(constraint => new Constraint(
+                noOfDecisionVariables,
+                constraint.coefficient.map(coefficient => new MinifiedNumber(coefficient.dividend,coefficient.divisor)),
+                constraint.symbol,
+                constraint.constant
+            )),
+            nonNegativeConstraints.map(constraint => new Constraint(
+                noOfDecisionVariables,
+                constraint.coefficient.map(coefficient => new MinifiedNumber(coefficient.dividend,coefficient.divisor)),
+                constraint.symbol,
+                new MinifiedNumber(constraint.constant.dividend,constraint.constant.divisor)
+            )),
+            htmlGenerator
+        );
+        let cost;
+        try{
+            cost = simplexSolver.solve();
+        }catch(error){
+            return res.status(400).json({
+                success: false,
+                message: "Error during calculations",
+                data: {}
+            });
+        }
+        return res.status(200).json({
+            success: true,
+            message: "Simplex Done",
+            data: {
+                cost,
+                htmlContent: htmlGenerator.getContent()
+            }
+        });
+    }
 }
--- server/routes/toolsRoute.js
+++ server/routes/toolsRoute.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const {calculateAssignment,calculateTransportation} = require("../controllers/calculators/OR");
+const {calculateAssignment,calculateTransportation, calculateSimplex} = require("../controllers/calculators/OR");
 
 router.post("/calc/or/assignment",calculateAssignment);
 router.post("/calc/or/transportation",calculateTransportation);
+router.post("/calc/or/simplex",calculateSimplex);
 
 module.exports = router;
