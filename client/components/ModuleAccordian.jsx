3b3589fb1434413d77a46529866e7ffb12fa1906
MOHANcoder <mohan235689@gmail.com>
2024-02-15 23:14:41 +0530

    🐞 fix at Module🪗

--- client/components/ModuleAccordian.jsx
+++ client/components/ModuleAccordian.jsx
@@ -1,40 +1,42 @@
 import { Add, ArrowDownward, ArrowUpward, Remove } from "@mui/icons-material";
 import { useState } from "react";
 
-export default function ModuleAccordian({ name, editable, chapters, detailsStyle,removeModule,id }) {
+const titleStyles = {
+    outline: 'none',
+    border: 'none',
+    width: '100%',
+    boxSizing: 'border-box',
+    padding: '5px 0px',
+    height: '50px',
+    fontSize: 'x-large'
+};
+
+const ChapterCard = ({ name, id, setCourseChapters, courseChapters }) => {
+    return (
+        <div style={{
+            boxShadow: '0px 0px 4px 1px grey',
+            width: '100%',
+            padding: '5px',
+            margin: '10px 0px',
+            display: 'flex'
+        }}>
+            <input type="text" style={{ ...titleStyles, fontSize: 'large' }} onChange={
+                (e) => setCourseChapters(pre => {
+                    const copiedArray = [...pre];
+                    copiedArray[id] = e.target.value;
+                    return copiedArray;
+                })
+            }
+                value={courseChapters[id]}
+            />
+            <button onClick={() => removeChapter(id)}><Remove /></button>
+        </div>
+    )
+};
+
+export default function ModuleAccordian({ name, editable, chapters, detailsStyle, removeModule, id }) {
     const [courseChapters, setCourseChapters] = useState(chapters);
     const [isModuleOpened, setIsModuleOpened] = useState(false);
-
-    const titleStyles = {
-        outline: 'none',
-        border: 'none',
-        width: '100%',
-        boxSizing: 'border-box',
-        padding: '5px 0px',
-        height: '50px',
-        fontSize: 'x-large'
-    };
-
-    const ChapterCard = ({ name, id }) => {
-        return (
-            <div style={{
-                boxShadow: '0px 0px 4px 1px grey',
-                width: '100%',
-                padding: '5px',
-                margin: '10px 0px',
-                display: 'flex'
-            }}>
-                <input type="text" defaultValue={name} style={{ ...titleStyles, fontSize: 'large' }} onChange={
-                    (e) => setCourseChapters(pre => {
-                        const copiedArray = [...pre];
-                        copiedArray[id] = e.target.value;
-                        return copiedArray;
-                    })
-                } />
-                <button onClick={() => removeChapter(id)}><Remove /></button>
-            </div>
-        )
-    };
 
     const addChapter = () => {
         setCourseChapters((pre) => [...pre, "new Chapter"])
@@ -51,13 +53,16 @@ export default function ModuleAccordian({ name, editable, chapters, detailsStyle
             // border:'1px solid',
             boxShadow: '0px 0px 2px 1px grey',
             padding: '10px'
-        }}>
+        }}
+        >
             <div style={{
                 display: 'flex'
-            }}>
+            }}
+                onClick={() => setIsModuleOpened(!isModuleOpened)}
+            >
                 {editable ? <input type="text" style={titleStyles} defaultValue={name} /> : <div style={titleStyles}>{name}</div>}
                 <button onClick={() => setIsModuleOpened(!isModuleOpened)}>{isModuleOpened ? <ArrowDownward /> : <ArrowUpward />}</button>
-                &nbsp;<button onClick={() => removeModule(id)}><Remove/></button>
+                &nbsp;<button onClick={() => removeModule(id)}><Remove /></button>
             </div>
             {isModuleOpened && <>
                 <div style={{
@@ -69,7 +74,10 @@ export default function ModuleAccordian({ name, editable, chapters, detailsStyle
                 <div style={{
                     padding: '5px 15px'
                 }}>
-                    {courseChapters.map((chapter, i) => <ChapterCard key={i} id={i} name={chapter} />)}
+                    {courseChapters.map((chapter, i) => <ChapterCard key={i} id={i} name={chapter}
+                        setCourseChapters={setCourseChapters}
+                        courseChapters={courseChapters}
+                    />)}
                 </div>
             </>}
         </div>
